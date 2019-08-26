0d9d6607581260b49dcea58fdbc78600fdb33643
nikola126 <nikj1705@gmail.com>
2019-08-26 17:26:59 +0300

    works, cleanup needed

--- dmpm.py
+++ dmpm.py
@@ -5,63 +5,67 @@ Created on Tue Aug 20 14:58:09 2019
 @author: Nikola
 """
 
-def dmpm(U,Y,E,na=0,nb=0,nc=0,intercept=0):
+def dmpm(U,Y,na = 0,nb = 0,intercept = 0):
     import numpy as np
+    # returns the matrix F
+    # E is not used
+    # assumes U,Y are not empty
+    N,r = Y.shape
     
-    # assumes U,Y,E are not empty
+    na = int(np.max(na))
+    nb = int(np.max(nb))
     
-    # a is a tuple of E's dimensions
-    a = E.shape
-    # N is E's rows; M is E's columns
-    # used later when initializing empty arrays
-    N = a[0]
-    M = a[1]
+    n = np.max(np.array([na,nb]))
     
-    na = np.max(na)
-    nb = np.max(nb)
-    nc = np.max(nc)
-    n = np.max(np.array([na,nb,nc]))
-    
-    
-    if intercept:
-        F = np.ones(((N-n),1))
-    else:
-        # WHAT SIZE ??
-        F = np.empty([N,M])
         
-    # in MATLAB several rows of each matrix (U,Y and E) are appended
-    # F is returned at the end
+    # in MATLAB several rows of each matrix (U and Y) are appended
+    # number of columns doubles (???) 
+    # [  starts at 1 ][  starts at 0  ]
+    #          ...         ...
+    # [ends at last-1][ends at last-2]
     
-    
-    # shape of Y to append
     rows = Y.shape[0]
     cols = Y.shape[1]
-    Y_to_append = np.empty([rows,cols])
+    Y_to_append_1 = np.empty([rows,cols])
+    Y_to_append_2 = np.empty([rows,cols])
     
-    for i in range(na,1,-1):
-        Y_to_append[i][:] = -Y[(n-na+i):(N-na+i-1)][:]
-    # sticks Y to F
-    np.concatenate(F,Y_to_append)
+    # first part
+    for i in range(na,0,-1):
+        Y_to_append_1 = -Y[(n-na+i):(N-na+i)][:]
+        if i != na:    
+            np.concatenate((Y_to_append_1,Y_to_append_1),axis = 1)
+    # second part
+    for i in range(na,0,-1):
+        Y_to_append_2 = -Y[(n-na+i-1):(N-na+i-1)][:]
+        if i != na:    
+            np.concatenate((Y_to_append_2,Y_to_append_2),axis = 1)
+            
+    F = np.concatenate((Y_to_append_1,Y_to_append_2),axis = 1)
+
+    # U doesnt run in MATLAB
+    if nb == 0 :
+        pass
+    else:
+        rows = U.shape[0]
+        cols = U.shape[1]
+        U_to_append_1 = np.empty([rows,cols])
+        U_to_append_2 = np.empty([rows,cols])
+        
+        # first part
+        for i in range(nb,0,-1):
+            U_to_append_1 = -U[(n-nb+i):(N-nb+i)][:]
+            if i != nb:    
+                np.concatenate((U_to_append_1,U_to_append_1),axis = 1)
+        # second part
+        for i in range(nb,0,-1):
+            U_to_append_2 = -U[(n-nb+i-1):(N-nb+i-1)][:]
+            if i != nb:    
+                np.concatenate((U_to_append_2,U_to_append_2),axis = 1)
+                
+        U = np.concatenate((U_to_append_1,U_to_append_2),axis = 1)    
+        F = np.concatenate((F,U),axis = 1)
     
-    # shape of U to append
-    rows = U.shape[0]
-    cols = U.shape[1]
-    U_to_append = np.empty([rows,cols])
-    
-    for i in range(nb,1,-1):
-        U_to_append[i][:] = U[(n-nb+i):(N-nb+i-1)][:]
-    # sticks U to F
-    np.concatenate(F,U_to_append)
-    
-    # shape of E to append
-    rows = E.shape[0]
-    cols = E.shape[1]
-    E_to_append = np.empty([rows,cols])
-    
-    for i in range(nc,1,-1):
-        E_to_append[i][:] = -E[(n-nc+i):(N-nc+i-1)][:]
-    # sticks E to F
-    np.concatenate(F,E_to_append)
+
         
     return F
     
