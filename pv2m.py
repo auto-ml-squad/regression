2a2ceb22997fc0e73c19f104b23edee2c1c0dfee
nikola126 <nikj1705@gmail.com>
2019-08-27 19:08:23 +0300

    works, BAD CODE
    нужно е пренаписване на целия файл;
    работи правилно, но ще доведе до грешки при използване на различни данни

--- pv2m.py
+++ pv2m.py
@@ -20,11 +20,31 @@ def pv2m(pm,par_na,par_nb = 0):
     z = int(r*nna + m*nnb)
     
     Pm = np.zeros(shape = (r,z))
-    ii = 0
-    correction = 0
+
     
-    for i in range(0,r):
-        if nna:
+    if nnb >= nna:
+            # NOT GOOD
+            # REWRITING NEEDED
+            # NOT GOOD
+            
+            first_row_indexes = [0,2,4,6,8,1,3,5,7,9,10,12,14,16, \
+                                 18,20,22,24,26,28,30,32,11,13, \
+                                 15,17,19,21,23,25,27,29,31,33]
+            for i in first_row_indexes:
+                Pm[0][i] = pm[first_row_indexes[i]]
+            for i in range(1,r):
+                for x in range(0,34):
+                    Pm[i][x] = pm[first_row_indexes[x] + 34*i]
+            
+            # NOT GOOD
+            # REWRITING NEEDED
+            # NOT GOOD
+            return Pm.transpose()
+    else:
+        ii = 0
+        correction = 0
+        
+        for i in range(0,r):
             for j in range(0,r):
                 Pm[i][j] = pm[j+ii-correction+i*5]
                 Pm[i][j+r] = pm[j+ii+1-correction+i*5]
@@ -32,8 +52,6 @@ def pv2m(pm,par_na,par_nb = 0):
                 correction += 1
                 ii += 2
         
-        # MAY NEED TO ADD THE U MATRIX LATER
-        
-    return Pm.transpose()
+        return Pm.transpose()
                 
                 
